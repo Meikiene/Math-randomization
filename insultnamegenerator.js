05ae2c1632056c90c8b4b3ca656885a1bf80504c
Meikiene <42394095+Meikiene@users.noreply.github.com>
2018-08-21 12:42:52 -1000

    Add files via upload
    Added a new file called insultnamegenerator.js

---
+++ insultnamegenerator.js
@@ -0,0 +1,14 @@
+//Create an array of adjectives or adverbs
+var adjWords = [
+"kind",
+"smart",
+"funny",
+"strong"
+];
+//Ask a person for his or her name
+var userAnswer = prompt("Enter First Name");
+//Generate a random number
+var x = Math.floor(Math.random() * adjWords.length);
+//Let the user know what kind of person are. For example,
+//username followed or preceded by the array item.
+window.alert( adjWords[x] + " " + userAnswer);
